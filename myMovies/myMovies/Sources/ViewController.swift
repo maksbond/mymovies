8a2d5fc6499551b8bc763b12bed470bf64c25c2a
maksbond <bondarmaksymwork@gmail.com>
2019-08-11 17:56:15 +0300

    Created project. Added files to gitignore.

---
+++ myMovies/myMovies/Sources/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  myMovies
+//
+//  Created by Maksym Bondar on 8/11/19.
+//  Copyright © 2019 Maksym Bondar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
